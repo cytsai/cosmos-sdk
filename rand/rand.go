e38662a2718a3f39941f21fc87bd9e0cc83f7a29
Kenyon Tsai <cytsai@gmail.com>
2021-05-10 03:15:45 +0800

    Int63n & Float64

--- rand/rand.go
+++ rand/rand.go
@@ -4,14 +4,12 @@ import (
         mrand "math/rand"
 	"os"
         "fmt"
-        "bufio"
-	"strings"
 	"runtime"
 	"testing"
-	"strconv"
+	"strings"
 )
 
-func printState(n int) {
+func printState(n int64) {
 	fmt.Printf("\n")
 	fmt.Printf("COVERAGE %g\n", testing.Coverage())
 	fmt.Printf("STATE %d ", n)
@@ -19,7 +17,7 @@ func printState(n int) {
 	frames := runtime.CallersFrames(pc[:(runtime.Callers(2, pc) - 2)])
 	for {
 		frame, more := frames.Next()
-		fmt.Printf("%d ", frame.PC) //("%d %s %s %d\n", frame.PC, frame.Function, frame.File, frame.Line)
+		fmt.Printf("%s;", strings.TrimPrefix(frame.Function, "github.com/cosmos/cosmos-sdk/"))
 		if !more {
 			break
 		}
@@ -27,25 +25,42 @@ func printState(n int) {
 	fmt.Printf("\n")
 }
 
-func getInt(guideRNG *bufio.Reader) int64 {
-	s, err := guideRNG.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-	n, err := strconv.ParseInt(strings.TrimSuffix(s, "\n"), 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	return n
-}
-
 func (r *Rand) Intn(n int) int {
-	if !r.guided {
+	if r.guide == nil {
 		return r.base.Intn(n)
 	}
+	printState(int64(n))
+	var rand int
+	if _, err := fmt.Fscanf(r.guide, "%d\n", &rand); err != nil {
+		panic(err)
+	}
+	fmt.Printf("ACTION %d\n", rand)
+	return rand
+}
+
+func (r *Rand) Int63n(n int64) int64 {
+	if r.guide == nil {
+		return r.base.Int63n(n)
+	}
 	printState(n)
-	rand := int(getInt(r.guideRNG))
-	//fmt.Printf("ACTION %d\n", rand)
+	var rand int64
+	if _, err := fmt.Fscanf(r.guide, "%d\n", &rand); err != nil {
+		panic(err)
+	}
+	fmt.Printf("ACTION %d\n", rand)
+	return rand
+}
+
+func (r *Rand) Float64() float64 {
+	if r.guide == nil {
+		return r.base.Float64()
+	}
+	printState(0)
+	var rand float64
+	if _, err := fmt.Fscanf(r.guide, "%g\n", &rand); err != nil {
+		panic(err)
+	}
+	fmt.Printf("ACTION %g\n", rand)
 	return rand
 }
 
@@ -53,15 +68,8 @@ func (r *Rand) Int63() int64 {
 	return r.base.Int63()
 }
 
-func (r *Rand) Int63n(n int64) int64 {
-	return r.base.Int63n(n)
-}
-
-func (r *Rand) Float64() float64 {
-	return r.base.Float64()
-}
-
 func (r *Rand) Perm(n int) []int {
+	// TODO: consider using Intn
 	return r.base.Perm(n)
 }
 
@@ -70,13 +78,13 @@ func (r *Rand) Read(p []byte) (n int, err error) {
 }
 
 func (r *Rand) GetMathRand() *mrand.Rand {
+	// TODO: check importance
 	return r.base
 }
 
 type Rand struct {
-	base     *mrand.Rand
-	guided   bool
-	guideRNG *bufio.Reader
+	base  *mrand.Rand
+	guide *os.File
 }
 
 type Source    = mrand.Source
@@ -93,10 +101,10 @@ func New(src Source) *Rand {
 	return &Rand{base: mrand.New(src)}
 }
 
-func NewGuided(src Source, guide string) *Rand {
-	guidePipe, err := os.Open(guide)
+func NewGuided(src Source, guidePath string) *Rand {
+	guide, err := os.Open(guidePath)
 	if err != nil {
 		panic(err)
 	}
-	return &Rand{base: mrand.New(src), guided: true, guideRNG: bufio.NewReader(guidePipe)}
+	return &Rand{base: mrand.New(src), guide: guide}
 }
